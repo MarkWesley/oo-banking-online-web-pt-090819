a3be07b8350c4e6e7771ecdc4bc859c11f51aaff
Mark Stone <markwstone85@gmail.com>
2019-10-11 14:29:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,14 @@ class Transfer
   end
 
   def execute_transaction
-
+    if @sender.balance > @amount && @status = "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
   end
 
 end
