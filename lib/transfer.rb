48130cd43a83c4d101515ae973041e30a174a04b
Mark Stone <markwstone85@gmail.com>
2019-10-11 14:37:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-  
+
     elsif @status = "rejected"
       "Transaction rejected. Please check your account balance."
     else
